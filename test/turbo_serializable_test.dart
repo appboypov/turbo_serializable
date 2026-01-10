7958e6ab000fc1498bc591b9c69bb4365b29f711
appboypov <info@codaveto.com>
2026-01-10 02:29:15 +0100

    chore: apply pub.dev best practices
    - Remove flutter_test dependency (pure Dart package)
    - Use test package instead of flutter_test in tests
    - Package scores 160/160 pub.dev points

--- test/turbo_serializable_test.dart
+++ test/turbo_serializable_test.dart
@@ -1,4 +1,4 @@
-import 'package:flutter_test/flutter_test.dart';
+import 'package:test/test.dart';
 import 'package:turbo_response/turbo_response.dart';
 import 'package:turbo_serializable/turbo_serializable.dart';
 
